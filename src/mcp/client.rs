d925c2f364033b1d2642303a2d713f84934450d4
Link Dupont <link@sub-pop.net>
2025-11-24 12:14:57 -0500

    Set version to 0.1.1

--- src/mcp/client.rs
+++ src/mcp/client.rs
@@ -390,7 +390,7 @@ mod tests {
             },
             client_info: Implementation {
                 name: "mcpcli".to_string(),
-                version: "0.1.0".to_string(),
+                version: env!("CARGO_PKG_VERSION").to_string(),
             },
         };
 
